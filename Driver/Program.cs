b8bc649b6ff0ac24e10b443b70e58a1a9d303052
Prof <mbdeir@helennow.com>
2025-09-18 13:10:53 -0400

    DRIVER

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -5,10 +5,12 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var MyDbV1 = new DbServer("DEV002", Environment.GetEnvironmentVariable("dev2cs"));
-        var MyDbV2 = new DbServer("Corewell", Environment.GetEnvironmentVariable("connectionString"));
-        string IndexPage = DbComparer.Compare(MyDbV1, MyDbV2, threadCount:20);
-        #region Optional:
+        var DEV002   = new DbServer("DEV002", Environment.GetEnvironmentVariable("dev2cs"));
+        var Corewell = new DbServer("Corewell", Environment.GetEnvironmentVariable("connectionString"));
+        var CMH      = new DbServer("CMH", Environment.GetEnvironmentVariable("cmhCs"));
+        
+        string IndexPage = DbComparer.Compare(DEV002,CMH, threadCount:20);
+        #region Optional
         // You can optionally pass any of the following parameters:
         // logger: your custom ILogger instance
         // outputFolder: path to save the results (string)
