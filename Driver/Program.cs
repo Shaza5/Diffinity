264a776cdea82ec6374509f055502561224e8399
Shaza5 <shazahaidar14@gmail.com>
2025-10-17 16:39:12 +0300

    FEAT: replace copy button with an icon

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -5,11 +5,11 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var k   = new DbServer("k", Environment.GetEnvironmentVariable("k"));
-        var s = new DbServer("s", Environment.GetEnvironmentVariable("s"));
-        //var CMH      = new DbServer("CMH", Environment.GetEnvironmentVariable("cmhCs"));
+        var DEV002 = new DbServer("DEV002", Environment.GetEnvironmentVariable("dev2cs"));
+        var Corewell = new DbServer("Corewell", Environment.GetEnvironmentVariable("connectionString"));
+        var CMH = new DbServer("CMH", Environment.GetEnvironmentVariable("cmhCs"));
 
-        string IndexPage = DbComparer.Compare(k, s);
+        string IndexPage = DbComparer.Compare(DEV002, CMH);
         #region Optional
         // You can optionally pass any of the following parameters:
         // logger: your custom ILogger instance
